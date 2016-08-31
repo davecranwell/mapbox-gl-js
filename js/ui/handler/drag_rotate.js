36249f2bbdca8a183b0a45b528c47d9c0dee5cb8
Scott Andrews <scott@mapbox.com>
2016-08-30 18:00:56 -0700

    Allow drag rotate handler to control pitch
    Right-click and drag previously only controlled the bearing of the map.
    Now it also controls the pitch along with bearing.
    
    Dragging up and down controls the pitch. Dragging left and right controls
    the bearing.
    
    The pitch control is opt-out and can be disabled by passing `pitchOnRotate:
    false` when initializing the Map. The experience can be demoed at
    http://localhost:9966/debug/rotate.html

--- js/ui/handler/drag_rotate.js
+++ js/ui/handler/drag_rotate.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var DOM = require('../../util/dom'),
-    Point = require('point-geometry'),
     util = require('../../util/util');
 
 module.exports = DragRotateHandler;
@@ -21,11 +20,13 @@ var inertiaLinearity = 0.25,
  * @param {Object} [options]
  * @param {number} [options.bearingSnap] The threshold, measured in degrees, that determines when the map's
  *   bearing (rotation) will snap to north.
+ * @param {bool} [options.pitchOnRotate=true] Control the map pitch in addition to the bearing
  */
 function DragRotateHandler(map, options) {
     this._map = map;
     this._el = map.getCanvasContainer();
     this._bearingSnap = options.bearingSnap;
+    this._pitch = options.pitchOnRotate !== false;
 
     util.bindHandlers(this);
 }
@@ -89,15 +90,6 @@ DragRotateHandler.prototype = {
         this._startPos = this._pos = DOM.mousePos(this._el, e);
         this._center = this._map.transform.centerPoint;  // Center of rotation
 
-        // If the first click was too close to the center, move the center of rotation by 200 pixels
-        // in the direction of the click.
-        var startToCenter = this._startPos.sub(this._center),
-            startToCenterDist = startToCenter.mag();
-
-        if (startToCenterDist < 200) {
-            this._center = this._startPos.add(new Point(-200, 0)._rotate(startToCenter.angle()));
-        }
-
         e.preventDefault();
     },
 
@@ -115,9 +107,10 @@ DragRotateHandler.prototype = {
 
         var p1 = this._pos,
             p2 = DOM.mousePos(this._el, e),
-            center = this._center,
-            bearingDiff = p1.sub(center).angleWith(p2.sub(center)) / Math.PI * 180,
+            bearingDiff = (p1.x - p2.x) * 0.8,
+            pitchDiff = (p1.y - p2.y) * -0.5,
             bearing = map.getBearing() - bearingDiff,
+            pitch = map.getPitch() - pitchDiff,
             inertia = this._inertia,
             last = inertia[inertia.length - 1];
 
@@ -125,6 +118,7 @@ DragRotateHandler.prototype = {
         inertia.push([Date.now(), map._normalizeBearing(bearing, last[1])]);
 
         map.transform.bearing = bearing;
+        if (this._pitch) map.transform.pitch = pitch;
 
         this._fireEvent('rotate', e);
         this._fireEvent('move', e);
